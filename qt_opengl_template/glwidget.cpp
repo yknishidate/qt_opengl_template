e6274353d38bea592398bec7a350c2d415447732
nishidate-yuki <asta18425@gmail.com>
2020-04-15 21:16:04 +0900

    clean:

--- qt_opengl_template/glwidget.cpp
+++ qt_opengl_template/glwidget.cpp
@@ -11,84 +11,28 @@ void GLWidget::initializeGL()
 {
     // QOpenGLFunctionsを現在のコンテキストに関連付けする
     initializeOpenGLFunctions();
-    // version確認
-    qDebug() << "GL version  :" << QLatin1String(reinterpret_cast<const char*>(glGetString(GL_VERSION)));
-    qDebug() << "GLSL version:" << QLatin1String(reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION)));
 
     // 背景を設定
-    glClearColor(0.7f, 0.7f, 0.4f, 1.0f);
-
-    // 頂点を用意
-    int numVertices = 3;
-    float vertices[] {
-         0.0f, 1.0f, 0.0f, 1.0f,
-        -1.0f, 0.0f, 0.0f, 1.0f,
-         1.0f, 0.0f, 0.0f, 1.0f,
-    };
-
-    // -----vertex bufferをセットアップする-----
-    // bufferを作成し、さらに現在のコンテキストにバインド
-//    vertexBuffer.create();
-    // bufferの用途を設定する
-//    vertexBuffer.setUsagePattern(QOpenGLBuffer::StaticDraw);
-    // bufferの領域確保を行い、さらにverticesデータを入れる
-//    const int numDimensions = 4;
-//    int bufferSize = numVertices * numDimensions * sizeof(float);
-//    vertexBuffer.allocate(vertices, bufferSize);
-
+    glClearColor(0.0f, 0.0f, 0.3f, 1.0f);
 
     // shader programをセットアップする
     shaderProgram.addShaderFromSourceFile(QOpenGLShader::Vertex, ":/vertex_shader.vsh");
     shaderProgram.addShaderFromSourceFile(QOpenGLShader::Fragment, ":/fragment_shader.fsh");
-    qDebug() << "link: " << shaderProgram.link();
-    qDebug() << "bind: " << shaderProgram.bind();
-
-    // locationを取得
-//    variables_location["position"] = 0;
-    variables_location["vpMatrix"] = shaderProgram.uniformLocation("vpMatrix");
-
+    shaderProgram.link();
+    shaderProgram.bind();
 }
 
 void GLWidget::paintGL()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // view matrix
-    QVector3D eyePos(0, 0, 5);
-    QVector3D targetPos(0, 0, 5);
-    QVector3D upVector(0, 1, 0);
-    viewMatrix.setToIdentity();
-    viewMatrix.lookAt(eyePos, targetPos, upVector);
-
-    // vp matrixをshaderに送る
-    QMatrix4x4 vpMatrix = projectionMatrix * viewMatrix;
-    shaderProgram.setUniformValue(variables_location["vpMatrix"], vpMatrix);
-
-    int numDimensions = 2;
+    // 頂点を用意
     static float vertices[] {
-         0.0f, 1.0f,
-        -1.0f, 0.0f,
-         1.0f, 0.0f,
+         0.0f,  0.5f,
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
     };
 
-    shaderProgram.enableAttributeArray(0);
-    shaderProgram.setAttributeArray(0, vertices, numDimensions);
+    shaderProgram.enableAttributeArray("position");
+    shaderProgram.setAttributeArray("position", vertices, /*numDimensions = */ 2);
 
-    glDrawArrays(GL_TRIANGLES, /*firstVertex=*/ 0, /*numVertex=*/ 3);
-}
-
-void GLWidget::resizeGL(int w, int h)
-{
-    // viewportの領域を設定
-//    glViewport( 0, 0, w, h);
-
-    // projection matrixを単位行列にリセット
-    projectionMatrix.setToIdentity();
-
-    // projection matrixのperspectiveを設定
-    float verticalAngle = 60.0;
-    float aspectRatio = float(w)/h;
-    float nearPlane = 0.01;
-    float farPlane = 1000.0;
-    projectionMatrix.perspective(verticalAngle, aspectRatio, nearPlane, farPlane);
+    glDrawArrays(GL_TRIANGLES, /*firstVertex = */ 0, /*numVertex = */ 3);
 }
--- qt_opengl_template/main.cpp
+++ qt_opengl_template/main.cpp
@@ -4,14 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    // set dark gui
-    QPalette darkPalette;
-    darkPalette.setColor(QPalette::Window, QColor(77,77,77));
-    a.setPalette(darkPalette);
-
     MainWindow w;
     w.show();
-
     return a.exec();
 }
