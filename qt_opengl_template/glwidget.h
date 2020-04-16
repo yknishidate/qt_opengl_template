71b36cd6d0a57ba985bc8daeafeebf20c9386c89
nishidate-yuki <asta18425@gmail.com>
2020-04-16 12:10:43 +0900

    update: glwidget.h

--- qt_opengl_template/glwidget.h
+++ qt_opengl_template/glwidget.h
@@ -2,9 +2,11 @@
 #define GLWIDGET_H
 
 #include <QWidget>
+#include <QtDebug>
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 #include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
 
 class GLWidget : public QOpenGLWidget, protected QOpenGLFunctions
 {
@@ -16,9 +18,14 @@ public:
 protected:
     void initializeGL() override;
     void paintGL() override;
+    void resizeGL(int w, int h) override;
 
 private:
+    QOpenGLBuffer vertexBuffer;
     QOpenGLShaderProgram shaderProgram;
+    QMatrix4x4 projectionMatrix;
+    QMatrix4x4 viewMatrix;
+    QMap<QString, int> variables_location;
 
 };
 #endif // GLWIDGET_H
